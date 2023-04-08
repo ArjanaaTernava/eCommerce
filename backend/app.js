4f34d7a5893eaf167776a34b2639bb72b567d752
Albin Hashani <albinhashani02@gmail.com>
2023-04-08 02:59:06 +0200

    add the first route and test it on postman

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+
+// Import route
+const products = require('./routes/products');
+
+app.use('/api/v1', products);
+
 module.exports = app;
