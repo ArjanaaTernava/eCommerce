d61791f2e00d2133b0d1e71902af41a5d3483173
Erza Osmani <94173748+erzaosmani@users.noreply.github.com>
2023-04-13 11:59:23 +0200

    Added encryption while registration

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
 // Import route
-const products = require('./routes/products');
+const products = require("./routes/products");
+const auth = require("./routes/auth");
 
-app.use('/api/v1', products);
+app.use("/api/v1", products);
+app.use("/api/v1", auth);
 
 module.exports = app;
