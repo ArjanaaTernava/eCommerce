bb7f143e109008abbe84b7aa95e1d6b81b5da6f2
ArjanaaTernava <arijanaternava1@gmail.com>
2023-04-23 13:16:12 +0200

    Admin routes for getting all users and specific user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,8 +10,10 @@ const {
   getUserProfile,
   updatePassword,
   updateProfile,
+  allUsers,
+  getUserDetails,
 } = require("../controllers/authController");
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const { isAuthenticatedUser,authorizeRoles } = require("../middlewares/auth");
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
@@ -20,4 +22,7 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/me").get(isAuthenticatedUser ,getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser,updatePassword);
 router.route('/me/update').put(isAuthenticatedUser,updateProfile);
+router.route('/admin/users').get(isAuthenticatedUser,authorizeRoles('admin'),allUsers)
+router.route('/admin/user/:id').get(isAuthenticatedUser,authorizeRoles('admin'),getUserDetails)
+
 module.exports = router;
