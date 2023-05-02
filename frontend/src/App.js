48c17ab62988e4f79346611bd45ff2fda1aa85e6
Erza Osmani <94173748+erzaosmani@users.noreply.github.com>
2023-05-02 21:00:23 +0200

    Added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import NavigationBar from "./components/layout/NavigationBar";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer"
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <div className="NavigationBar">
         <NavigationBar></NavigationBar>
         <Header />
+        <Footer />
       </div>
     </>
   );
