8bb99b665c963e7e265c7b909675befd0863cb2b
ArjanaaTernava <arijanaternava1@gmail.com>
2023-05-04 21:47:05 +0200

    Added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import NavigationBar from "./components/layout/NavigationBar";
 import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer"
-
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <div className="NavigationBar">
-        <NavigationBar></NavigationBar>
         <Header />
+        <NavigationBar></NavigationBar>
+        <Home />
         <Footer />
       </div>
     </>
