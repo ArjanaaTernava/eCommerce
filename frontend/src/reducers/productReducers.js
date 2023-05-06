52778edad998abe0a0c3b463dc8495c59f9fdd17
Erza Osmani <94173748+erzaosmani@users.noreply.github.com>
2023-05-06 17:58:27 +0200

    Added Product Details Reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,41 +1,72 @@
-import { CLEAR_ERRORS } from '../constants/productConstants'
-import {    
-    ALL_PRODUCTS_REQUEST,
-    ALL_PRODUCTS_SUCCESS,
-    ALL_PRODUCTS_FAIL
-
-} from '../constants/productConstants'
+import {
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  CLEAR_ERRORS,
+} from "../constants/productConstants";
 
 export const productsReducer = (state = { products: [] }, action) => {
-    switch(action.type){
-        
-        case ALL_PRODUCTS_REQUEST:
-            return{
-                loading: true,
-                procuts: []
-            }
+  switch (action.type) {
+    case ALL_PRODUCTS_REQUEST:
+      return {
+        loading: true,
+        procuts: [],
+      };
 
-        case ALL_PRODUCTS_SUCCESS:
-            return{
-                loading: false,
-                procuts: action.payload.products,
-                productsCount: action.payload.productsCount
-            }
-        
-        case ALL_PRODUCTS_FAIL:
-            return {
-                loading: false,
-                error: action.payload
-            }
-        
-        case CLEAR_ERRORS:
-            return {
-                ...state,
-                error: null
-            }
-                
+    case ALL_PRODUCTS_SUCCESS:
+      return {
+        loading: false,
+        procuts: action.payload.products,
+        productsCount: action.payload.productsCount,
+      };
 
-        default:
-            return state;
-    }
-}
+    case ALL_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false, //fetching from back end
+        product: action.payload,
+      };
+
+    case PRODUCT_DETAILS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
