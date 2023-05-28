c1418ec2883dae911ac4b6e02a758f29f31015ce
ArjanaaTernava <arijanaternava1@gmail.com>
2023-05-28 22:32:11 +0200

    Added delete category functionality.

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -31,3 +31,19 @@ exports.getCategoryById = catchAsyncErrors(async (req, res, next) => {
     category,
   });
 });
+
+// Delete category  =>  /api/v1/admin/category/delete/:id
+exports.deleteCategory = catchAsyncErrors(async (req, res, next) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    return next(new ErrorHandler("Category not found", 404));
+  }
+
+  await category.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Category was deleted successfully!",
+  });
+});
