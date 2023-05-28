05b9e7c8c3181bd3049faca899caad3a37837439
ArjanaaTernava <arijanaternava1@gmail.com>
2023-05-28 22:34:16 +0200

    Added delete category router.

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createCategory,getCategories, getCategoryById
+  createCategory,
+  getCategories,
+  getCategoryById,
+  deleteCategory,
 } = require("../controllers/categoryController");
 
 const { isAuthenticatedUser } = require("../middlewares/auth");
@@ -10,5 +13,8 @@ const { isAuthenticatedUser } = require("../middlewares/auth");
 router.route("/admin/categories").get(isAuthenticatedUser, getCategories);
 router.route("/admin/category/:id").get(isAuthenticatedUser, getCategoryById);
 router.route("/admin/category/add").post(isAuthenticatedUser, createCategory);
+router
+  .route("/admin/category/delete/:id")
+  .delete(isAuthenticatedUser, deleteCategory);
 
 module.exports = router;
