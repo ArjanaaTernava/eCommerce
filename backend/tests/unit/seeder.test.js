929883a4da9a1a39a8223118d0960a23bdb107fc
ArditZubaku <zubaku92@gmail.com>
2023-05-29 18:24:03 +0200

    Added 2 unit tests,  for seedSellers and for seedData.

--- backend/tests/unit/seeder.test.js
+++ backend/tests/unit/seeder.test.js
@@ -1,45 +1,42 @@
-/* 
-
-Here are three unit tests in this file. Each test case corresponds to one of the seeding functions (seedProducts, seedBrands, seedCategories).
-
-Here's a breakdown of the tests:
-
-Test 1: "should seed data for products"
-
-This test case verifies the behavior of the seedProducts function.
-It checks that the deleteMany and insertMany methods of the Product model are called once, and the expected log messages are printed to the console.
-Test 2: "should seed data for brands"
-
-This test case verifies the behavior of the seedBrands function.
-It checks that the deleteMany and insertMany methods of the Brand model are called once, and the expected log messages are printed to the console.
-Test 3: "should seed data for categories"
-
-This test case verifies the behavior of the seedCategories function.
-It checks that the deleteMany and insertMany methods of the Category model are called once, and the expected log messages are printed to the console.
-
-*/
-
 const chai = require("chai");
 const sinon = require("sinon");
 const Product = require("../../models/product");
 const Brand = require("../../models/brands");
 const Category = require("../../models/category");
+const Seller = require("../../models/seller");
 const seeder = require("../../utils/seeder");
 
 const expect = chai.expect;
 
+/* 
 
+Here are 5 different tests for the corresponding seeding functions. 
+
+Here's a breakdown of the test:
+The tests are written using the chai assertion library and the sinon library for mocking and stubbing.
+
+The test file focuses on testing the seeding functions for different data types, such as products, brands, categories, and sellers. It verifies that these functions perform the necessary database operations, such as deleting existing data and inserting new data.
+
+Each test case sets up mock objects for the database models (Product, Brand, Category, Seller) and uses them to assert that the expected methods are called. It also stubs the console.log method to check if the appropriate log statements are outputted during the seeding process.
+
+The test cases ensure that the Seeder functions work correctly and perform the expected actions, such as deleting existing data, adding new data, and logging relevant information.
+
+Overall, this test file helps validate the correctness of the data seeding functionality in the application, ensuring that the database is properly populated with the required data.
+
+*/
 
 describe("Seeder", () => {
   let productModelMock;
   let brandModelMock;
   let categoryModelMock;
+  let sellerModelMock;
 
   beforeEach(() => {
     // Create mocks for the models
     productModelMock = sinon.mock(Product);
     brandModelMock = sinon.mock(Brand);
     categoryModelMock = sinon.mock(Category);
+    sellerModelMock = sinon.mock(Seller);
   });
 
   afterEach(() => {
@@ -47,6 +44,7 @@ describe("Seeder", () => {
     productModelMock.restore();
     brandModelMock.restore();
     categoryModelMock.restore();
+    sellerModelMock.restore();
   });
 
   it("should seed data for products", async () => {
@@ -95,6 +93,58 @@ describe("Seeder", () => {
     expect(insertManyMock.calledOnce).to.be.true;
     expect(consoleLogStub.calledWith("Categories are deleted")).to.be.true;
     expect(consoleLogStub.calledWith("All Categories are added.")).to.be.true;
+
+    // Restore the stubbed console.log method
+    consoleLogStub.restore();
+  });
+
+  it("should seed data for sellers", async () => {
+    const deleteManyMock = sellerModelMock.expects("deleteMany").resolves();
+    const insertManyMock = sellerModelMock.expects("insertMany").resolves();
+    const consoleLogStub = sinon.stub(console, "log");
+
+    await seeder.seedSellers();
+
+    // Assertions
+    expect(deleteManyMock.calledOnce).to.be.true;
+    expect(insertManyMock.calledOnce).to.be.true;
+    expect(consoleLogStub.calledWith("Sellers are deleted")).to.be.true;
+    expect(consoleLogStub.calledWith("All Sellers are added.")).to.be.true;
+
+    // Restore the stubbed console.log method
+    consoleLogStub.restore();
+  });
+
+  it("should seed all data", async () => {
+    const deleteManyProductMock = productModelMock.expects("deleteMany").resolves();
+    const insertManyProductMock = productModelMock.expects("insertMany").resolves();
+    const deleteManyBrandMock = brandModelMock.expects("deleteMany").resolves();
+    const insertManyBrandMock = brandModelMock.expects("insertMany").resolves();
+    const deleteManyCategoryMock = categoryModelMock.expects("deleteMany").resolves();
+    const insertManyCategoryMock = categoryModelMock.expects("insertMany").resolves();
+    const deleteManySellerMock = sellerModelMock.expects("deleteMany").resolves();
+    const insertManySellerMock = sellerModelMock.expects("insertMany").resolves();
+    const consoleLogStub = sinon.stub(console, "log");
+
+    await seeder.seedData();
+
+    // Assertions
+    expect(deleteManyProductMock.calledOnce).to.be.true;
+    expect(insertManyProductMock.calledOnce).to.be.true;
+    expect(deleteManyBrandMock.calledOnce).to.be.true;
+    expect(insertManyBrandMock.calledOnce).to.be.true;
+    expect(deleteManyCategoryMock.calledOnce).to.be.true;
+    expect(insertManyCategoryMock.calledOnce).to.be.true;
+    expect(deleteManySellerMock.calledOnce).to.be.true;
+    expect(insertManySellerMock.calledOnce).to.be.true;
+    expect(consoleLogStub.calledWith("Products are deleted")).to.be.true;
+    expect(consoleLogStub.calledWith("All Products are added.")).to.be.true;
+    expect(consoleLogStub.calledWith("Brands are deleted")).to.be.true;
+    expect(consoleLogStub.calledWith("All Brands are added.")).to.be.true;
+    expect(consoleLogStub.calledWith("Categories are deleted")).to.be.true;
+    expect(consoleLogStub.calledWith("All Categories are added.")).to.be.true;
+    expect(consoleLogStub.calledWith("Sellers are deleted")).to.be.true;
+    expect(consoleLogStub.calledWith("All Sellers are added.")).to.be.true;
 
     // Restore the stubbed console.log method
     consoleLogStub.restore();
